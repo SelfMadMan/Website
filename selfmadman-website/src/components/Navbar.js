2487308bbddb2fb02e64f2273dda391037867dd1
Pauline Mila Alonso <113476814+Milhaal@users.noreply.github.com>
2024-03-14 14:14:42 +0100

    front

--- selfmadman-website/src/components/Navbar.js
+++ selfmadman-website/src/components/Navbar.js
@@ -1,5 +1,29 @@
-export default function Navbar() {
-    return (
-        <h1>Navbar</h1>
-    )
-}
+import React, { useState } from 'react';
+import './Navbar.css';
+import './Root.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <nav className="navbar">
+    <div className={`nav-items ${isOpen && 'open'}`}>
+        <div className='navbar-content'>
+            <a href="#">Presentation</a>
+            <a href="#">Gameplay</a>
+            <a href="#">Newsletter</a>
+        </div>  
+        <button href="" className='cta-btn'>Get Notified</button>
+    </div>
+
+      
+      
+      <button className="nav-toggle" onClick={() => setIsOpen(!isOpen)}>
+        <i className={isOpen ? 'bi bi-x-lg' : 'bi bi-list'}></i>
+      </button>
+    </nav>
+  );
+};
+
+export default Navbar;
