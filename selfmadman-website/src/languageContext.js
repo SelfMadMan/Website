af5594a8b746fbd407879e08828fea26586cb24e
Haxxxxxx <62444678+Haxxxxxx@users.noreply.github.com>
2024-03-17 21:29:56 +0100

    Auto translate

--- selfmadman-website/src/languageContext.js
+++ selfmadman-website/src/languageContext.js
@@ -1,19 +1,31 @@
-// src/contexts/LanguageContext.js
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState, useEffect } from 'react';
 
 const LanguageContext = createContext();
 
 export const useLanguage = () => useContext(LanguageContext);
 
 export const LanguageProvider = ({ children }) => {
-    const [language, setLanguage] = useState('EN'); // Default language
-  
-    console.log("Current Language:", language); // Add this line to check current language state
-  
+    // Detect browser language, slice to get the first part (e.g., 'en-US' becomes 'en')
+    const defaultLanguage = navigator.language.slice(0, 2) === 'fr' ? 'FR' : 'EN';
+    
+    const [language, setLanguage] = useState(defaultLanguage); // Use detected language as default
+
+    useEffect(() => {
+        // Optionally, you could store the user's language preference in localStorage and retrieve it here
+        const storedLanguage = localStorage.getItem('appLanguage');
+        if (storedLanguage) {
+            setLanguage(storedLanguage);
+        }
+    }, []);
+
+    // Optionally save language preference when it changes
+    useEffect(() => {
+        localStorage.setItem('appLanguage', language);
+    }, [language]);
+
     return (
       <LanguageContext.Provider value={{ language, setLanguage }}>
         {children}
       </LanguageContext.Provider>
     );
-  };
-  
+};
