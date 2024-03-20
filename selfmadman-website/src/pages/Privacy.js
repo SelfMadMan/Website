29958932b99c1d3a032020dbb721160e70391e29
Pauline Mila Alonso <113476814+Milhaal@users.noreply.github.com>
2024-03-20 12:33:19 +0100

    push pricay

--- selfmadman-website/src/pages/Privacy.js
+++ selfmadman-website/src/pages/Privacy.js
@@ -3,10 +3,95 @@ import Navbar from "../components/Navbar";
 
 export default function Privacy() {
     return (
-        <>
+        <div className="privacy-bg">
             <Navbar />
-            <h2>Privacy</h2>
+            <div className="privacy-content">
+      <h1>Politique de Confidentialité</h1>
+      <p>La présente politique de confidentialité (ci-après la “Politique de Confidentialité”) définit les conditions de traitement des données personnelles par SELF MAD MAN société par actions simplifiée ayant son siège social en France (ci-après “Self Mad Man”) concernant les internautes navigant sur le Site Internet et ses Clients (ci-après les “Utilisateurs”).</p>
+      <p> Les termes apparaissant avec une lettre majuscule initiale non définis dans la Politique de Confidentialité, le sont dans les Conditions Générales de vente et d'utilisation de la Société.</p>
+        <p> Les données personnelles de l'Utilisateur sont traitées par Self Mad Man en tant que responsable de traitement. Self Mad Man se conforme en toutes circonstances aux réglementations qui lui sont 
+applicables en matière de protection des données personnelles notamment aux dispositions de la loi Informatique et Libertés n° 78-17 du 6 janvier 1978 modifiée et au Règlement UE 2016/679 du 
+Parlement européen et du Conseil du 27 avril 2016 relatif à la protection des personnes physiques à l'égard du traitement des données à caractère personnel et à la libre circulation de ces données (ci
+après ensemble la “ Réglementation Données Personnelles").</p>
+
+      <h2>1. CATÉGORIES DE DONNÉES TRAITÉES</h2>
+      <p>Les données personnelles concernées par les traitements mis en œuvre dans le cadre des présentes sont :</p>
+      <ul>
+        <li>des données d'identification telles que les nom, prénom, email, adresse, autres ;</li>
+        <li>des données liées aux interactions de l'Utilisateur avec le Site Internet ;</li>
+        <li>des données liées aux comportements d'achat ; et</li>
+        <li>des données de localisation.</li>
+      </ul>
+
+      <h2>2. FINALITÉS DE TRAITEMENTS</h2>
+      <p>Les informations relatives au Compte Client sont traitées pour créer et gérer le Compte Client. La base légale de ce traitement est le consentement, qui peut être retiré à tout moment. La communication des données personnelles de l'Utilisateur est toutefois indispensable pour la création d'un Compte Client. Le refus de communiquer ces données entraîne l'impossibilité de créer le Compte Client.</p>
+      <p>Les informations relatives aux commandes de Produits et/ou Services sont collectées à des fins de traitement et suivi de commandes, ainsi qu'à des fins de prévention de la fraude, d'analyses statistiques, et de connaissance client. La base légale de ce traitement est les Conditions générales de vente et d'utilisation. La communication des données personnelles de l'Utilisateur est indispensable pour passer une commande sur le Site Internet. Le refus de communiquer ces données entraîne l'impossibilité de traiter toute commande.</p>
+      
+      <h2>3. DESTINATAIRES DES DONNÉES PERSONNELLES</h2>
+      <p>Les données personnelles sont hébergées par un prestataire situé en dehors de l'Union européenne. Self Mad Man garantit avoir mis en place des mesures afin d'assurer un niveau de protection adéquat au sens de la Réglementation Données Personnelles.</p>
+      <p>L'Utilisateur est informé que Self Mad Man peut également partager ses données personnelles avec des organismes publics ou autorités en faisant la demande, pour se conformer à une obligation légale ou protéger des droits, et avec de potentiels futurs acquéreurs, dans le respect de la Réglementation Données Personnelles.</p>
+      <p>Sous-traitants Ultérieurs
+ Certaines données personnelles peuvent être transmises à des partenaires de Self Mad Man (les “
+ ”). Les Sous-traitants Ultérieurs de Self Mad Man disposent de 
+politiques de confidentialité propres auxquelles l'Utilisateur est invité à se référer. En tout état de 
+cause, ces partenaires ne sont autorisés à utiliser les Données Personnelles que dans le cadre 
+des activités décrites ci-dessus qu'ils effectuent pour le compte de Self Mad Man et uniquement 
+pour la réalisation des finalités prévues aux présentes.</p>
+<p>Self Mad Man informe l'Utilisateur de tout ajout ou modification des Sous-traitants Ultérieurs 
+intervenant sur le périmètre des traitements qu'elle réalise via la mise à jour de sa Politique de 
+confidentialité. L'Utilisateur est invité à consulter régulièrement la présente Politique de 
+confidentialité et à faire part de toute objection portant sur un Sous-traitant Ultérieur, par écrit et 
+dans les meilleurs délais.</p>
+
+<h2>4. DURÉE DE CONSERVATION DES DONNÉES</h2>
+<p>Les données personnelles des Utilisateurs sont conservées uniquement le temps nécessaire à la 
+réalisation de la finalité pour laquelle Self Mad Man a collecté ces données, afin de répondre aux 
+besoins des Utilisateurs ou pour remplir ses obligations légales ou réglementaires d'archivage.</p>
+<p>Pour établir la durée de conservation des données personnelles, Self Mad Man applique les 
+critères suivants :</p>
+<ul>
+    <li>en cas de commande de Produits et/ou Services : les données personnelles sont 
+conservées pour la durée de la relation contractuelle et trois ans après la collecte ou le 
+dernier contact avec l'Utilisateur, à des fins de prospection commerciale ;</li>
+<li>Self Mad Man est susceptible de conserver certaines données afin de remplir ses 
+obligations légales ou réglementaires en matière d'archivage afin de lui permettre d'
+ exercer ses droits et/ou à des fins statistiques ou historiques.</li>
+</ul>
+<p> A l'issue des durées mentionnées ci-dessus, les données personnelles seront supprimées ou Self 
+Mad Man procédera à leur anonymisation.</p>
+
+<h2>5. DROITS DES PERSONNES CONCERNÉES</h2>
+<p> L'Utilisateur dispose d'un droit d'accès, de rectification, de suppression (effacement), de 
+portabilité de ses données personnelles, de limitation du traitement ainsi qu'un droit d'
+ opposition au traitement de ses données collectées et traitées par Self Mad Man, en contactant 
+directement Self Mad Man à l'adresse email suivante : j.selfmadman@gmail.com</p>
+<p> En cas de réclamation, l'Utilisateur peut contacter la CNIL qui est l'autorité compétente en 
+matière de protection des données personnelles, dont voici les coordonnées : 3 Place de 
+Fontenoy, 75007 Paris, téléphone : 01 53 73 22 22.</p>
+
+<h2>6. GESTION DES COOKIES</h2>
+<p> Les cookies sont des informations placées par un site web sur l'équipement d'un utilisateur 
+lorsqu'il visite le Site Internet. Un cookie ne contient pas nécessairement le nom ou l'adresse e
+mail de l'Utilisateur.</p>
+<p>Les cookies sont utilisés afin de stocker des informations relatives à la navigation du terminal de l'
+ Utilisateur sur le Site Internet, telles que les pages visitées par l'Utilisateur ou la date et l'heure de 
+la visite. Cela permet à la page d'afficher des informations adaptées aux besoins de l'Utilisateur.</p>
+<p> Le Site Internet utilise la technologie des « cookies » qui permet le traitement statistique des 
+données et fournit des informations détaillées concernant notamment le nombre d'Utilisateurs 
+ayant eu accès au Site Internet, les modalités d'accès au Site Internet et le nombre d'accès de 
+chaque Utilisateur au Site Internet.</p>
+<p> Self Mad Man utilise différents types de cookies répondant à différents besoins :</p>
+<p> L'installation de certains cookies est soumise au consentement de l'Utilisateur, lorsqu'ils n'ont pas 
+pour finalité exclusive de permettre ou faciliter la navigation sur le Site Internet ou lorsqu'ils ne 
+sont pas strictement nécessaires à la fourniture des Produits et/ou Services, à la gestion de la 
+relation avec l'Utilisateur. Lors de la première visite sur le Site Internet, l'Utilisateur est informé 
+par un bandeau des conditions de dépôt des cookies sur son terminal, il peut les refuser ou les 
+accepter librement.</p>
+<p> L'Utilisateur peut toujours refuser le stockage de cookies en configurant son navigateur. La 
+désactivation complète des cookies indispensables peut faire obstacle à l'utilisation de toutes les 
+fonctionnalités interactives du Site Internet.</p>
+    </div>
             <Footer />
-        </>
+        </div>
     )
 }
--- selfmadman-website/src/pages/Terms.js
+++ selfmadman-website/src/pages/Terms.js
@@ -3,10 +3,39 @@ import Navbar from "../components/Navbar";
 
 export default function Terms() {
     return (
-        <>
+        <div className="privacy-bg">
             <Navbar />
-            <h2>Terms</h2>
+            <div className="privacy-content">
+      <h1>Conditions Générales de Vente et d'Utilisation Self Mad Man</h1>
+
+      <p> Les présentes conditions générales de vente (ci-après les “Conditions Générales") représentent le socle
+ unique de la relation commerciale entre les Parties.</p>
+ <p>Elles ont pour objet de définir les conditions dans lesquelles la société 
+SELF MAD MAN
+ actions simplifiée au capital social de 2 000 €, (ci-après “
+ Self Mad Man
+ , société par 
+”) fournit au client (ci-après le “
+ Client
+ ”), les produits tels que décrits à l’article 3 ci-dessous (ci-après les “Produits ”).</p>
+<p> Self Mad Man et le Client étant ci-après désignés ensembles les “
+ Parties
+ 
+ ” et individuellement une “
+ Partie ”.</p>
+
+      <h2>1. MENTIONS LÉGALES</h2>
+      <p> https://selfmadman.fr/ (ci-après le “
+ Site Internet
+ ”) est édité par :  SELF MAD MAN, société par actions simplifiée au capital social de 2 000 €. </p>
+<p>Hébergeur du site internet : Firebase, Google LLC, 1600 Amphitheatre Parkway, Mountain View, 
+California 94043 USA, -, https://firebase.google.com/</p>
+
+
+<h2>2. CHAMP D’APPLICATION</h2>
+
+    </div>
             <Footer />
-        </>
+        </div>
     )
 }
